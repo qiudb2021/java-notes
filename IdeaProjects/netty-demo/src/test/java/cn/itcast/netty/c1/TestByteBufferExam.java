cc0851ca3b53d65a45e0a58d512440eb3ca41bb6
qiudb2021 <qiudb2022@outlook.com>
2022-06-09 14:09:34 +0800

    netty-bytebuffer

--- IdeaProjects/netty-demo/src/test/java/cn/itcast/netty/c1/TestByteBufferExam.java
+++ IdeaProjects/netty-demo/src/test/java/cn/itcast/netty/c1/TestByteBufferExam.java
@@ -1,5 +1,9 @@
 package cn.itcast.netty.c1;
 
+import java.nio.ByteBuffer;
+
+import static cn.itcast.netty.c1.ByteBufferUtil.debugAll;
+
 public class TestByteBufferExam {
     public static void main(String[] args) {
         /*
@@ -13,5 +17,32 @@ public class TestByteBufferExam {
             w are you?\n
         现在要求将错乱的数据恢复成原始数据
          */
+
+        ByteBuffer source = ByteBuffer.allocate(32);
+        source.put("Hello,World\nI'm zhangsan\nHo".getBytes());
+        debugAll(source);
+
+        split(source);
+        source.put("w are you?\n".getBytes());
+        split(source);
+    }
+
+    private static void split(ByteBuffer source) {
+        source.flip();
+        for (int i = 0; i < source.limit(); i++) {
+            // 找到一个完整的消息
+            if(source.get(i) == '\n') {
+                int length = i + 1 - source.position();
+                // 把这条完整消息存入新的 ByteBuffer;
+                ByteBuffer target = ByteBuffer.allocate(length);
+                // 从 source 读，向 target 写
+                for (int j = 0; j < length; j++) {
+                    target.put(source.get());
+                }
+                target.flip();
+                debugAll(target);
+            }
+        }
+        source.compact();
     }
 }
