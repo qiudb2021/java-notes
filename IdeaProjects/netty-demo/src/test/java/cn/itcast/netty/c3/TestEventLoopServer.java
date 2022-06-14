55310b4398888a2d12586cc05ff88f9169a49ab5
qiudb2021 <qiudb2022@outlook.com>
2022-06-14 15:05:22 +0800

    netty-eventLoopGroup

---
+++ IdeaProjects/netty-demo/src/test/java/cn/itcast/netty/c3/TestEventLoopServer.java
@@ -0,0 +1,35 @@
+package cn.itcast.netty.c3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+@Slf4j
+public class TestEventLoopServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override                                         // ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
