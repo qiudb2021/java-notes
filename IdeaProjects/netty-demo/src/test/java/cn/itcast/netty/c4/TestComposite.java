277aa3421e38c2da8551f385be836a3560113252
qiudb2021 <qiudb2022@outlook.com>
2022-06-16 20:21:33 +0800

    netty-ByteBuf-Composite

---
+++ IdeaProjects/netty-demo/src/test/java/cn/itcast/netty/c4/TestComposite.java
@@ -0,0 +1,23 @@
+package cn.itcast.netty.c4;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+
+import static cn.itcast.netty.c4.TestByteBuf.log;
+
+public class TestComposite {
+    public static void main(String[] args) {
+        ByteBuf buf1 = ByteBufAllocator.DEFAULT.buffer();
+        buf1.writeBytes(new byte[] {1,2,3,4,5});
+
+        ByteBuf buf2 = ByteBufAllocator.DEFAULT.buffer();
+        buf2.writeBytes(new byte[] {6,7,8,9,10});
+
+        CompositeByteBuf buf = ByteBufAllocator.DEFAULT.compositeDirectBuffer();
+        buf.addComponents(true, buf1, buf2);
+
+        buf.retain();
+        log(buf);
+    }
+}
